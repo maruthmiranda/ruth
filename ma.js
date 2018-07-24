9a2cb9d2ee8b897e7ed77208a952726bee022952
maruthmiranda <41600579+maruthmiranda@users.noreply.github.com>
2018-07-24 10:01:36 +0800

    Update ma.js

--- ma.js
+++ ma.js
@@ -1,1 +1,65 @@
-
+[
+  {
+    "id": 145,
+    "name": "Grape Farm",
+    "imageUrl": "https://raw.githubusercontent.com/maruthmiranda/ruth/master/New%20folder/Grape.jpg",
+    "price": 250,
+    "priceCurrency": "₱",
+    "lat": 16.568704,
+    "lng": 120.322061
+  },
+  {
+    "id": 148,
+    "name": "Botanical Garden",
+    "imageUrl": "https://raw.githubusercontent.com/maruthmiranda/ruth/master/New%20folder/botanical.jpg",
+    "price": 200,
+    "priceCurrency": "₱",
+    "lat": 16.544041,
+    "lng": 120.324377
+  },
+  {
+    "id": 201,
+    "name": "Pugad Launion",
+    "imageUrl": "https://raw.githubusercontent.com/jethrew/jabien/master/qwerty/h3.jpg",
+    "price": 200,
+    "priceCurrency": "₱",
+    "lat": 16.336609,
+    "lng": 120.478857
+  },
+  {
+    "id": 205,
+    "name": "Halo-halo de iloko",
+    "imageUrl": "https://raw.githubusercontent.com/maruthmiranda/ruth/master/New%20folder/halo.jpg",
+    "price": 300,
+    "priceCurrency": "₱",
+    "lat": 16.657479,
+    "lng": 120.320558
+  },
+  {
+    "id": 211,
+    "name": "Macho Temple",
+    "imageUrl": "https://raw.githubusercontent.com/maruthmiranda/ruth/master/New%20folder/macho.jpg",
+    "price": 600,
+    "priceCurrency": "₱",
+    "lat": 16.608991,
+    "lng": 120.286336
+  },
+    {
+    "id": 212,
+    "name": "Pindangan Ruins",
+    "imageUrl": "https://raw.githubusercontent.com/maruthmiranda/ruth/master/New%20folder/pindangan.jpg",
+    "price": 499,
+    "priceCurrency": "₱",
+    "lat": 16.621112,
+    "lng": 120.317487
+  },
+   {
+    "id": 213,
+    "name": "ThunderBird",
+    "imageUrl": "https://raw.githubusercontent.com/maruthmiranda/ruth/master/New%20folder/thunderbird.jpg",
+    "price": 350,
+    "priceCurrency": "₱",
+    "lat": 16.565222,
+    "lng": 120.323073
+  }
+]
